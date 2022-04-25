b535c94e8ecf71d7c500797a0ef8d52e349fcea1
Toshi7088 <104213986+Toshi7088@users.noreply.github.com>
2022-04-25 11:36:01 +0530

    Add files via upload

---
+++ Outlook/src/main/contact/OutlookContactCreateTest.java
@@ -0,0 +1,13 @@
+package contact;
+
+import java.io.IOException;
+
+public class OutlookContactCreateTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		 Runtime.getRuntime().exec("C:\\Users\\rdpuser\\Desktop\\Auto It\\Outlook COntact Create Test.exe");
+ 
+	}
+
+}
---
+++ Outlook/src/main/task/OutlookTaskPresentPastFutureCreateTest.java
@@ -0,0 +1,14 @@
+package task;
+
+import java.io.IOException;
+
+public class OutlookTaskPresentPastFutureCreateTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		  Runtime.getRuntime().exec("C:\\Users\\rdpuser\\Desktop\\Auto It\\Outlook Task Create Present Past Future Test.exe");
+		
+		
+	}
+	
+}
