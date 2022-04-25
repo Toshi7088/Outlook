4a2f284fbc1d44032f7f734fbeff71efdca588dd
rdpuser <rdpuser@host.docker.internal>
2022-04-25 09:51:27 +0530

    First Commit

---
+++ Outlook/src/appointment/OutlookAppointmentPresentPastFutureCreateTest.java
@@ -0,0 +1,15 @@
+package appointment;
+
+import java.io.IOException;
+
+public class OutlookAppointmentPresentPastFutureCreateTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		
+		 Runtime.getRuntime().exec("C:\\Users\\rdpuser\\Desktop\\Auto It\\Outlook Appointment Create Present Past Future Test.exe");
+		
+	}
+
+}
